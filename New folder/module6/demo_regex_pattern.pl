e2b9e8bb970519b3a8c9ac268de702190fc5e853
saravanan75 <43355335+saravanan75@users.noreply.github.com>
2022-03-17 16:56:14 -0500

    Add files via upload

---
+++ New folder/module6/demo_regex_pattern.pl
@@ -0,0 +1,39 @@
+# Demonstrate Regex pattern matching in Perl
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my $dirname = 'c:\perl\logs\\';
+chomp($dirname);
+print("Navigating the directory $dirname \n");
+opendir(my $dirhandle, $dirname);
+
+my $outputfile = $dirname;
+my $outputlog = 'output.txt';
+my $writefile = $outputfile.$outputlog;
+
+open(my $wfh, '>>', $writefile);
+print $writefile;
+
+while(my $files = readdir($dirhandle)) {
+	if((substr $files, -4) eq ".log") {
+		print "Opening file $files\n";
+		chomp($files);
+		my $fn = $dirname.$files;
+		print("About to read $fn");
+		open(my $FH, '<', $fn) or die $!;
+		
+		while(<$FH>) {
+			print "$_";
+		}
+		print "Closing File $files \n";
+		close($FH);
+	}
+}
+close($wfh);
+
+
+
+
---
+++ New folder/module9/chipcard.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+package chipcard;
+
+use strict;
+use warnings;
+our @ISA = qw(card);
